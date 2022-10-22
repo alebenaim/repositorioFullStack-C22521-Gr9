8193512c7a42c8a2f1e2c876d9a0fd81541c79c9
VirginiaDiaz <vicky.4713.vd@gmail.com>
2022-10-21 23:13:38 -0300

    ahoraSi

--- js/form.js
+++ js/form.js
@@ -5,7 +5,6 @@ for(var i=0; i<inputs.length; i++){
             this.nextElementSibling.classList.add('fijar');
         }
     })
-}
 
 
 
@@ -13,19 +12,19 @@ const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    tel: /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/,
 }
 
-function validar() {
-var nombre, apellido, provincia, email, mensaje,expresion;
+function validacion() {
+var nombre, apellido, provincia, email, mensaje, tel;
 nombre= document.getElementById("nombre_contacto").value;
 apellido= document.getElementById("apellido_contacto").value;
-provincia= document.getElementById("provincias").value;
+provincia= document.getElementById("provincias");
 email= document.getElementById("email").value;
 mensaje= document.getElementById("mensaje").value;
-expresion= /\w+@\w+.+[a-z]/;
+tel= document.getElementById("telefono").value;
 
-
-if (nombre ==="" || apellido === "" || provincia === "" || email === "" || mensaje === ""){
+if (nombre === "" || apellido === "" || provincia === "" || email === "" || mensaje === ""){
     alert ("Todos los campos son obligatorios");
     return false;
 } 
@@ -48,5 +47,10 @@ else if (email.length>40){
 else if (!expresiones.correo.test(email)){
     alert("Correo invalido. ejemplo@dominio.com");
     return false;
+}
+else if (!expresiones.tel.test(tel)){
+    alert("Numero de telefono invalido");
+    return false;
+}
 }
 }
