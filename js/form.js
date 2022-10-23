cc87b6b73b9ef12eb5b37f02e01f7c62e52b0ea2
VirginiaDiaz <94577943+VirginiaDiaz@users.noreply.github.com>
2022-10-22 22:19:49 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -5,36 +5,41 @@ for(var i=0; i<inputs.length; i++){
             this.nextElementSibling.classList.add('fijar');
         }
     })
-}
-
 
 
 const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    tel: /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/,
 }
 
-function validar() {
-var nombre, apellido, provincia, email, mensaje,expresion;
+function validacion() {
+var nombre, apellido, provincia, email, mensaje, tel, parrafo;
 nombre= document.getElementById("nombre_contacto").value;
 apellido= document.getElementById("apellido_contacto").value;
-provincia= document.getElementById("provincias").value;
+provincia= document.getElementById("provincias");
 email= document.getElementById("email").value;
 mensaje= document.getElementById("mensaje").value;
-expresion= /\w+@\w+.+[a-z]/;
+tel= document.getElementById("telefono").value;
+parrafo= document.getElementById("warnings");
 
-
-if (nombre ==="" || apellido === "" || provincia === "" || email === "" || mensaje === ""){
-    alert ("Todos los campos son obligatorios");
+if (nombre === "" || apellido === "" || provincia === "" || email === "" || mensaje === ""){
+    parrafo.innerHTML +=`<i class="fa-sharp fa-solid fa-triangle-exclamation"></i><b><i>Todos los campos son requeridos<b> <br>`
     return false;
 } 
 else if (!expresiones.nombre.test(nombre)){
-    alert("El nombre solo debe contener letras");
+    swal({
+        text:'Este campo solo debe contener letras',
+        icon:'error',
+    });
     return false;
 }
 else if (!expresiones.apellido.test(apellido)){
-    alert("El apellido solo debe contener letras");
+    swal({
+        text:'Este campo solo debe contener letras',
+        icon:'error',
+    });
     return false;
 }
 else if (apellido.length>20){
@@ -46,7 +51,26 @@ else if (email.length>40){
     return false;
 }
 else if (!expresiones.correo.test(email)){
-    alert("Correo invalido. ejemplo@dominio.com");
+    swal({
+        text:'Correo invalido. ejemplo@dominio.com',
+        icon:'error',
+    });
+   
     return false;
+}
+else if (!expresiones.tel.test(tel)){
+    swal({
+        text:'Teléfono invalido. Por favor ingrese un número valido',
+        icon:'error',
+    });
+    return false;
+}
+else{
+    swal({
+        text:'Mensaje enviado con éxito',
+        icon:'success',
+        time:1500,
+    });
+}
 }
 }
